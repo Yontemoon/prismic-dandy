34e2efc036e96858a6d8b6d9fa15c0d3ea8d3794
YonteMoon <monteyoon1998@gmail.com>
2024-07-08 13:33:35 -0700

    Added header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Nunito, Nunito_Sans } from "next/font/google";
 import clsx from "clsx";
 import "./globals.css";
 import { createClient } from "@/prismicio";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const nunito = Nunito({
   subsets: ['latin'],
@@ -20,13 +22,13 @@ export async function generateMetadata(): Promise<Metadata> {
  
   const client = createClient()
 
-  const page = await client.getSingle("settings")
+  const settings = await client.getSingle("settings")
 
   return {
-    title: page.data.site_title || "Dandy fallback",
-    description: page.data.meta_descriptions || "Dandy is dandy...",
+    title: settings.data.site_title || "Dandy fallback",
+    description: settings.data.meta_descriptions || "Dandy is dandy...",
     openGraph: {
-      images: [page.data.og_image.url || ""],
+      images: [settings.data.og_image.url || ""],
     },
   }
 }
@@ -39,9 +41,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
       <body>
-        <header>Header!</header>
+        <Header />
         {children}
-        <footer>footer!</footer>
+        <Footer/>
 
       </body>
     </html>
