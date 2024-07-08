4c61d63cbd562490e231ae1078be77e4417bf59f
YonteMoon <monteyoon1998@gmail.com>
2024-07-08 12:32:42 -0700

    feat: Create settings custom types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1 className="font-body text-5xl">Hello world!!</h1>
-     {/* <h1 className="font-display text-5xl">Hello world!!</h1> */}
      
     </main>
   );
